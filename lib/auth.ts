6fe97e73a490d2aeda61eb92e66d0e8a0cbcf458
Abayomi Emmanuel Oluwaseyi <117934139+G305-dev@users.noreply.github.com>
2026-09-10 03:16:46 +0100

    Change default ADMIN_PASSWORD to an empty string

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 
 export const ADMIN_COOKIE = "fny_admin";
 export const ADMIN_SESSION_MAX_AGE = 60 * 5;
-export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "FluffyAdmin1";
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "";
 
 /**
  * Sandbox-preview escape hatch: some browsers block ALL third-party cookies
